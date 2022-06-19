6c3c4d697042da4096592eebe70ae2745bd50895
Sasha Ajintarev <77781886+sasha-ajin@users.noreply.github.com>
2022-06-19 14:05:00 +0300

    edit App.py to be more readable

--- App.py
+++ App.py
@@ -16,15 +16,11 @@ def main():
     santa_claus.hire_gnome(gnome2)
     santa_claus.hire_gnome(gnome3)
 
-    santa_claus.add_toy_to_list("Bicycle")
-    santa_claus.add_toy_to_list("Bicycle")
-    santa_claus.add_toy_to_list("Doll")
-    santa_claus.add_toy_to_list("Bicycle")
+    santa_claus.add_toy_to_list("Bicycle")  # Bicycle was created by gnome1 John
+    santa_claus.add_toy_to_list("Bicycle")  # Bicycle was created by gnome2 Bill
+    santa_claus.add_toy_to_list("Doll")  # Doll was created by gnome3 Steve
+    santa_claus.add_toy_to_list("Bicycle")  # Bicycle was created by gnome1 John
 
-    # Bicycle was created by gnome1 John
-    # Bicycle was created by gnome2 Bill
-    # Doll was created by gnome3 Steve
-    # Bicycle was created by gnome1 John
 
 if __name__ == "__main__":
     main()
